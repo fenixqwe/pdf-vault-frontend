59519eacac15b3055432deebfc8b03b7961b781b
fenixqwe <alexanderkornevua@gmail.com>
2025-07-16 21:46:47 +0300

    create zod validation for reset password

--- src/components/pages/authorization/resetPassword/stepTwo/StepTwo.tsx
+++ src/components/pages/authorization/resetPassword/stepTwo/StepTwo.tsx
@@ -1,22 +1,40 @@
+import {z} from "zod";
 import {toast} from "sonner";
 
-import {useEffect, useState} from "react";
+import {useEffect} from "react";
 import {useNavigate} from "react-router-dom";
+import {useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
 
 import AuthService from "@/services/AuthService.ts";
 
-import MyInput from "@/components/common/MyInput/MyInput.tsx";
 import {Button} from "@/components/ui/button.tsx";
+import {Form} from "@/components/ui/form.tsx";
+import MyFormInput from "@/components/common/MyFormInput/MyFormInput.tsx";
 
 import logo from "@/assets/pdf_logo.svg"
 import confirmLogo from "@/assets/confirmPasswordIcon.svg"
 import arrowIcon from "@/assets/arrow_return_btn_icon.svg";
 
 function StepTwo() {
-    const [newPassword, setNewPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
 
     const navigate = useNavigate();
+
+    const passwordSchema = z.object({
+        newPassword: z.string().min(6, "Password must be at least 6 characters"),
+        confirmPassword: z.string().min(6, "Confirmation must be at least 6 characters")
+    }).refine(data => data.newPassword === data.confirmPassword, {
+        message: "Passwords do not match",
+        path: ["confirmPassword"]
+    });
+
+    const form = useForm<z.infer<typeof passwordSchema>>({
+        resolver: zodResolver(passwordSchema),
+        defaultValues: {
+            newPassword: "",
+            confirmPassword: ""
+        }
+    });
 
     useEffect(() => {
         if (!window.location.href?.includes('resetPassword/newPassword?token=')) {
@@ -24,19 +42,20 @@ function StepTwo() {
         }
     }, []);
 
-    const changePasswordPromise = () => new Promise(async (resolve, reject) => {
+    const changePasswordPromise = (values: z.infer<typeof passwordSchema>) => new Promise(async (resolve, reject) => {
         try {
             const resetToken = window.location.href?.split('resetPassword/newPassword?token=')[1];
-            const response = await AuthService.resetPassword(resetToken, newPassword);
+            const response = await AuthService.resetPassword(resetToken, values.newPassword);
             navigate('/resetPassword/passwordUpdated');
             resolve(response.data.message);
+            form.reset();
         } catch (e: any) {
             reject(e.response.data.message);
         }
     })
 
-    async function changePassword() {
-        toast.promise(changePasswordPromise(), {
+    async function changePassword(values: z.infer<typeof passwordSchema>) {
+        toast.promise(changePasswordPromise(values), {
             loading: 'Changing password...',
             success: (data: any) => data,
             error: (errorMessage) => errorMessage,
@@ -61,23 +80,34 @@ function StepTwo() {
                             </p>
                         </div>
                     </div>
-                    <div className={'step-two-form'}>
-                        <div>
-                            <MyInput label={'New password'} value={newPassword} changeValue={setNewPassword}
-                                     id={"passwordInput"}
-                                     type={"password"} name={"password"}/>
-                        </div>
-                        <div>
-                            <MyInput label={'New password confirmation'} value={confirmPassword} changeValue={setConfirmPassword}
-                                     id={"passwordConfirmInput"}
-                                     type={"password"} name={"password"}/>
-                        </div>
-                        <div>
-                            <Button className={'w-full text-[14px] cursor-pointer h-[45px] bg-[#D85888] hover:!bg-[#847BEF] transition-all duration-500'} onClick={changePassword}>
-                                Change password
-                            </Button>
-                        </div>
-                    </div>
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(changePassword)} className={'flex flex-col gap-[10px]'}>
+                            <MyFormInput label={'New password'} name={"newPassword"} form={form} type={"password"} />
+                            <MyFormInput label={'New password confirmation'} name={"confirmPassword"} form={form} type={"password"} />
+                            <div className={'mt-[10px]'}>
+                                <Button type={'submit'} className={'w-full text-[14px] cursor-pointer h-[45px] bg-[#D85888] hover:!bg-[#847BEF] transition-all duration-500'}>
+                                    Change password
+                                </Button>
+                            </div>
+                        </form>
+                    </Form>
+                    {/*<div className={'step-two-form'}>*/}
+                    {/*    <div>*/}
+                    {/*        <MyInput label={'New password'} value={newPassword} changeValue={setNewPassword}*/}
+                    {/*                 id={"passwordInput"}*/}
+                    {/*                 type={"password"} name={"password"}/>*/}
+                    {/*    </div>*/}
+                    {/*    <div>*/}
+                    {/*        <MyInput label={'New password confirmation'} value={confirmPassword} changeValue={setConfirmPassword}*/}
+                    {/*                 id={"passwordConfirmInput"}*/}
+                    {/*                 type={"password"} name={"password"}/>*/}
+                    {/*    </div>*/}
+                    {/*    <div>*/}
+                    {/*        <Button className={'w-full text-[14px] cursor-pointer h-[45px] bg-[#D85888] hover:!bg-[#847BEF] transition-all duration-500'} onClick={changePassword}>*/}
+                    {/*            Change password*/}
+                    {/*        </Button>*/}
+                    {/*    </div>*/}
+                    {/*</div>*/}
                 </div>
             </div>
             <div className={'flex justify-center w-full'}>
